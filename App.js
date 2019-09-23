66abe70000a554bf4ea5de22bb5fc75c85cb6a1a
rosf73 <47631768+rosf73@users.noreply.github.com>
2019-09-24 00:11:46 +0900

    check log in from AsyncStorage

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
-import { StyleSheet, View, Text, Image, AsyncStorage } from 'react-native';
-//import AsyncStorage from '@react-native-community/async-storage';
+import { StyleSheet, View, Text, Image, TouchableOpacity } from 'react-native';
+import AsyncStorage from '@react-native-community/async-storage';
 import FastImage from 'react-native-fast-image';
 import { InMemoryCache } from 'apollo-cache-inmemory';
 import { persistCache } from 'apollo-cache-persist';
@@ -12,14 +12,13 @@ import apolloClientOptions from './apollo';
 const App = () => {
   const [ loaded, setLoaded ] = useState(false);
   const [ client, setClient ] = useState(null);
+  const [ isLoggedIn, setIsLoggedIn ] = useState(null);
   const preLoad = async () => {
     try {
-      const remoteImages = [
-        
-      ];
+      const remoteImages = [];
       const localImages = [
-        require('./resources/images/test.png'),
-        require('./resources/images/test2.png')
+        require("./resources/images/test.png"),
+        require("./resources/images/test2.png")
       ];
       await FastImage.preload(remoteImages.map(image => ({
         uri: image
@@ -38,6 +37,14 @@ const App = () => {
         ...apolloClientOptions
       });
 
+      const isLoggedIn = await AsyncStorage.getItem("isLoggedIn");
+      if(isLoggedIn === null || isLoggedIn === "false") {
+        setIsLoggedIn(false);
+      }
+      else {
+        setIsLoggedIn(true);
+      }
+
       setLoaded(true);
       setClient(client);
     } catch(e) {
@@ -49,15 +56,38 @@ const App = () => {
     preLoad();
   }, []);
 
-  return loaded && client ?
+  const logUserIn = async () => {
+    try {
+      await AsyncStorage.setItem("isLoggedIn", "true");
+      setIsLoggedIn(true);
+    } catch(e) {
+      console.log(e);
+    }
+  }
+
+  const logUserOut = async () => {
+    try {
+      await AsyncStorage.setItem("isLoggedIn", "false");
+      setIsLoggedIn(false);
+    } catch(e) {
+      console.log(e);
+    }
+  }
+
+  return loaded && client && isLoggedIn !== null ?
     <ApolloProvider client={client}>
       <View style={styles.container}>
-        <Image source={require('./resources/images/test2.png')}/>
+        { isLoggedIn === true ?
+          <TouchableOpacity onPress={logUserOut}><Text>logOut</Text></TouchableOpacity>
+          :
+          <TouchableOpacity onPress={logUserIn}><Text>logIn</Text></TouchableOpacity>
+        }
       </View>
     </ApolloProvider>
     :
     <View style={styles.container}>
-      <Image source={require('./resources/images/test.png')}/>
+      <Image source={require("./resources/images/test2.png")}/>
+      <Text>로딩중</Text>
     </View>
 };
 
