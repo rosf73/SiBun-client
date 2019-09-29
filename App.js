51d02b2cd7eda1fb7683923f5d7711151e875b0b
rosf73 <47631768+rosf73@users.noreply.github.com>
2019-09-29 18:51:08 +0900

    로그인 테스트

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { StyleSheet, View, Text, Image, TouchableOpacity } from 'react-native';
+import { StyleSheet, View, Image, Text } from 'react-native';
 import AsyncStorage from '@react-native-community/async-storage';
 import FastImage from 'react-native-fast-image';
 import { InMemoryCache } from 'apollo-cache-inmemory';
@@ -8,6 +8,7 @@ import ApolloClient from 'apollo-boost';
 import { ApolloProvider } from 'react-apollo-hooks';
 
 import apolloClientOptions from './apollo';
+import NavController from './components/NavController';
 
 const App = () => {
   const [ loaded, setLoaded ] = useState(false);
@@ -76,18 +77,12 @@ const App = () => {
 
   return loaded && client && isLoggedIn !== null ?
     <ApolloProvider client={client}>
-      <View style={styles.container}>
-        { isLoggedIn === true ?
-          <TouchableOpacity onPress={logUserOut}><Text>logOut</Text></TouchableOpacity>
-          :
-          <TouchableOpacity onPress={logUserIn}><Text>logIn</Text></TouchableOpacity>
-        }
-      </View>
+      <NavController/>
     </ApolloProvider>
     :
     <View style={styles.container}>
-      <Image source={require("./resources/images/test2.png")}/>
-      <Text>로딩중</Text>
+      <Image style={styles.logo} source={require("./resources/images/logo.png")}/>
+      <Text style={styles.loading}>loading...</Text>
     </View>
 };
 
@@ -96,7 +91,15 @@ const styles = StyleSheet.create({
     flex: 1,
     alignItems: 'center',
     justifyContent: 'center',
-    backgroundColor: '#FFF'
+    backgroundColor: '#E2937B'
+  },
+  logo: {
+    height: '18%',
+    resizeMode: 'contain'
+  },
+  loading: {
+    fontSize: 15,
+    color: '#FFF'
   }
 });
 
--- components/NavController.js
+++ components/NavController.js
@@ -1,6 +1,17 @@
 import React from 'react';
-import { Text } from 'react-native';
+import { View, Text, StyleSheet } from 'react-native';
 
 export default () => {
-  return <Text>NavController</Text>
+  return <View style={styles.container}>
+    <Text>Here is NavController</Text>
+  </View>
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: '#FFF'
+  }
+});
