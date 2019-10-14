c85602ae2a4075bb12b51fe698b04aadc415aa7f
rosf73 <47631768+rosf73@users.noreply.github.com>
2019-10-14 22:53:44 +0900

    file clean up

---
+++ sources/navigations/MainNavigation.js
@@ -0,0 +1,8 @@
+import { createAppContainer } from 'react-navigation';
+import { createStackNavigator } from 'react-navigation-stack';
+
+const MainNavigation = createStackNavigator({
+  
+});
+
+export default createAppContainer(MainNavigation);
