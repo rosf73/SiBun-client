9c44f8f0a736bdc67af1ac578189f805e38abef5
rosf73 <47631768+rosf73@users.noreply.github.com>
2019-10-15 19:32:15 +0900

    Main Screen 간단한 레이아웃 디자인

--- sources/screens/MainScreen.js
+++ sources/screens/MainScreen.js
@@ -1,17 +1,75 @@
 import React, { Component } from 'react';
-import { View, StyleSheet } from 'react-native';
+import { View, StyleSheet, TextInput, PermissionsAndroid } from 'react-native';
 import MapView, { PROVIDER_GOOGLE } from 'react-native-maps';
+import Geolocation from 'react-native-geolocation-service';
 
 class MainScreen extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      latitude: 0,
+      longitude: 0,
+      hasLocationPermission: false,
+      isLoaded: false
+    };
+  }
+
+  requestLocationPermission = async () => {
+    try {
+      const granted = await PermissionsAndroid.request(
+        PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION,
+        {
+          'title': 'Example App',
+          'message': 'Example App access to your location '
+        }
+      );
+
+      if (granted === PermissionsAndroid.RESULTS.GRANTED) {
+        this.setState({ hasLocationPermission: true });
+      } else {
+        console.log("location permission denied");
+        alert("Location permission denied");
+      }
+    } catch (err) {
+      console.warn(err);
+    }
+  }
+
+  async componentDidMount() {
+    await this.requestLocationPermission();
+
+    if (this.state.hasLocationPermission) {
+      Geolocation.getCurrentPosition(
+        (position) => {
+          this.setState({ latitude: position.coords.latitude });
+          this.setState({ longitude: position.coords.longitude });
+        },
+        (error) => {
+          // See error code charts below.
+          console.log(error.code, error.message);
+        },
+        { enableHighAccuracy: true, timeout: 15000, maximumAge: 10000 }
+      );
+    }
+  }
+
   render() {
     return (
       <View style={styles.container}>
+        <View style={styles.header}>
+          <TextInput
+            style={styles.textInput}
+            onChangeText={(number)=>{this.setState({number})}}
+            value={this.state.number}
+            placeholder=""
+            placeholderTextColor="#FFFFFF"/>
+        </View>
         <MapView
           provider={PROVIDER_GOOGLE} // remove if not using Google Maps
           style={styles.map}
           region={{
-            latitude: 37.78825,
-            longitude: -122.4324,
+            latitude: this.state.latitude,
+            longitude: this.state.longitude,
             latitudeDelta: 0.015,
             longitudeDelta: 0.0121,
           }}>
@@ -24,12 +82,21 @@ class MainScreen extends Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: 'center',
+    alignItems: 'stretch',
     justifyContent: 'center',
     backgroundColor: '#FFF'
   },
+  header: {
+    height: 55,
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    alignItems: 'center',
+    backgroundColor: '#FFF',
+    borderBottomWidth: 1,
+    borderBottomColor: '#CCC'
+  },
   map: {
-    ...StyleSheet.absoluteFillObject,
+    flex: 1
   }
 });
 
