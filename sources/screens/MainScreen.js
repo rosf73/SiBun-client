dd1281c445b3699a56541b16a6243ed4384774ff
rosf73 <47631768+rosf73@users.noreply.github.com>
2019-10-15 19:05:42 +0900

    Google Maps API 연결

--- sources/screens/MainScreen.js
+++ sources/screens/MainScreen.js
@@ -1,11 +1,21 @@
 import React, { Component } from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, StyleSheet } from 'react-native';
+import MapView, { PROVIDER_GOOGLE } from 'react-native-maps';
 
 class MainScreen extends Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text>MainScreen</Text>
+        <MapView
+          provider={PROVIDER_GOOGLE} // remove if not using Google Maps
+          style={styles.map}
+          region={{
+            latitude: 37.78825,
+            longitude: -122.4324,
+            latitudeDelta: 0.015,
+            longitudeDelta: 0.0121,
+          }}>
+        </MapView>
       </View>
     );
   }
@@ -17,6 +27,9 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
     backgroundColor: '#FFF'
+  },
+  map: {
+    ...StyleSheet.absoluteFillObject,
   }
 });
 
